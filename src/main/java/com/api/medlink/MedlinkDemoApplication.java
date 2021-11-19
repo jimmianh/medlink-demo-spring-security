d3f35bcb17f2d5a908af53400ebca5cc27ff72ac
jimmianh <jimmianh@1807@gmail.com>
2021-11-19 19:02:59 +0700

    hello

---
+++ src/main/java/com/api/medlink/MedlinkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.api.medlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlinkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedlinkDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/medlink/MedlinkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.medlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlinkDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
